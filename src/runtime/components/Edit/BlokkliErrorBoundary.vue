57964230018a4c4f78814296575d085d88d5334f
Jan Hug <me@dulnan.net>
2024-03-05 13:55:29 +0100

    Change style of error boundary overlay

--- src/runtime/components/Edit/BlokkliErrorBoundary.vue
+++ src/runtime/components/Edit/BlokkliErrorBoundary.vue
@@ -2,41 +2,43 @@
   <slot></slot>
 
   <Teleport to="body">
-    <div
-      v-if="errors.length"
-      class="bk bk-fatal-error-overlay"
-      @wheel.passive.stop
-      @click.stop
-      @mousedown.stop
-      @touchstart.passive.stop
-      @mousemove.stop
-    >
-      <div class="bk-fatal-error-overlay-info">
-        <Icon name="dead" />
-        <div>
-          <h2>{{ $t('fatalErrorTitle', 'blökkli has stopped working') }}</h2>
-          <p>
-            {{
-              $t(
-                'fatalErrorText',
-                'Unfortunately blökkli has encountered a fatal error which prevents it from working normally. You may be able to continue using it, but things may not work as expected.',
-              )
-            }}
-          </p>
-          <button class="bk-button" @click="errors = []">
-            {{ $t('fatalErrorButton', 'Continue anyway...') }}
-          </button>
+    <Transition name="bk-fade">
+      <div
+        v-if="errors.length"
+        class="bk bk-fatal-error-overlay"
+        @wheel.capture.passive.stop
+        @click.stop
+        @mousedown.stop
+        @touchstart.passive.stop
+        @mousemove.stop
+      >
+        <div class="bk-fatal-error-overlay-info">
+          <Icon name="sad" />
+          <div>
+            <h2>{{ $t('fatalErrorTitle', 'blökkli has stopped working') }}</h2>
+            <p>
+              {{
+                $t(
+                  'fatalErrorText',
+                  'Unfortunately blökkli has encountered a fatal error which prevents it from working normally. You may be able to continue using it, but things may not work as expected.',
+                )
+              }}
+            </p>
+            <button class="bk-button bk-is-danger" @click="errors = []">
+              {{ $t('fatalErrorButton', 'Continue anyway...') }}
+            </button>
+          </div>
+        </div>
+        <div class="bk-fatal-error-overlay-list">
+          <div v-for="(e, i) in unique" :key="i">
+            <h3>{{ e.name }}: {{ e.message }}</h3>
+            <code>
+              <pre>{{ e.stack }}</pre>
+            </code>
+          </div>
         </div>
       </div>
-      <div class="bk-fatal-error-overlay-list">
-        <div v-for="(e, i) in unique" :key="i">
-          <h3>{{ e.name }}: {{ e.message }}</h3>
-          <code>
-            <pre>{{ e.stack }}</pre>
-          </code>
-        </div>
-      </div>
-    </div>
+    </Transition>
   </Teleport>
 </template>
 
