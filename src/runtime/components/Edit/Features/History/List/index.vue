8fe2dcc7f1cdae197dfa85fc151dd4243e18e1d4
Jan Hug <me@dulnan.net>
2024-11-22 12:27:40 +0100

    refactor: move history list to separate component

---
+++ src/runtime/components/Edit/Features/History/List/index.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="bk bk-history bk-control">
+    <ul v-if="mapped.length">
+      <li
+        v-for="item in mapped"
+        :key="item.index"
+        :class="{
+          'bk-is-not-active': item.index > currentMutationIndex,
+          'bk-is-active': item.index === currentMutationIndex,
+          'bk-is-disabled': !item.enabled,
+          'bk-is-applied': item.index < currentMutationIndex && item.enabled,
+        }"
+      >
+        <button
+          :disabled="!canEdit"
+          class="bk-history-item-button"
+          @click="setHistoryIndex(item.index)"
+        >
+          <div>
+            <div>
+              <strong>{{ item.mutation.plugin?.label }}</strong>
+            </div>
+            <RelativeTime
+              v-if="item.timestamp"
+              v-slot="{ formatted }"
+              :timestamp="item.timestamp"
+            >
+              <em>{{ formatted }}</em>
+            </RelativeTime>
+          </div>
+        </button>
+        <div v-if="canSetStatus" class="bk-history-item-actions">
+          <button
+            @click.prevent="setMutationItemStatus(item.index, !item.enabled)"
+          >
+            <Icon name="close" />
+          </button>
+        </div>
+      </li>
+      <li v-if="totalMutations > showAmount" class="bk-history-load-more">
+        <button class="bk-history-item-button" @click="showAmount += 100">
+          <strong
+            >{{
+              $t('historyShowMore', 'Show @count more').replace(
+                '@count',
+                Math.min(totalMutations - showAmount, 100).toString(),
+              )
+            }}
+          </strong>
+        </button>
+      </li>
+      <li
+        class="bk-is-last"
+        :class="[
+          currentMutationIndex === -1 ? 'bk-is-active' : 'bk-is-applied',
+          { 'bk-has-shadow': !scrolledToEnd },
+        ]"
+      >
+        <button class="bk-history-item-button" @click="setHistoryIndex(-1)">
+          <div>
+            <strong>{{
+              $t('historyCurrentRevision', 'Current revision')
+            }}</strong>
+          </div>
+          <!-- @TODO: Pass in the timestamp of the entity's latest revision. -->
+          <!-- <RelativeTime -->
+          <!--   v-if="item.timestamp" -->
+          <!--   :timestamp="item.timestamp" -->
+          <!--   v-slot="{ formatted }" -->
+          <!-- > -->
+          <!--   <div>{{ formatted }}</div> -->
+          <!-- </RelativeTime> -->
+        </button>
+      </li>
+    </ul>
+    <div v-else class="bk-history-empty-message">
+      {{ $t('historyEmpty', 'There are now changes yet.') }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, useBlokkli, watch } from '#imports'
+import { RelativeTime, Icon } from '#blokkli/components'
+import type { MutationItem } from '#blokkli/types'
+
+defineProps<{
+  scrolledToEnd: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'setIndex', data: number): void
+}>()
+
+const { state, $t, adapter } = useBlokkli()
+
+const { mutations, currentMutationIndex, canEdit, mutateWithLoadingState } =
+  state
+
+const canSetStatus = !!adapter.setMutationItemStatus
+
+const showAmount = ref(50)
+const totalMutations = computed(() => mutations.value.length)
+
+watch(totalMutations, (newTotal, previousTotal) => {
+  if (newTotal !== previousTotal) {
+    showAmount.value = 50
+  }
+})
+
+type HistoryItem = {
+  index: number
+  mutation: MutationItem
+  timestamp: number
+  enabled: boolean
+}
+
+const mapped = computed<HistoryItem[]>(() =>
+  mutations.value
+    .map((mutation, index) => {
+      return {
+        index,
+        mutation,
+        timestamp: mutation.timestamp ? Number.parseInt(mutation.timestamp) : 0,
+        enabled: mutation.enabled !== false,
+      }
+    })
+    .sort((a, b) => {
+      return b.timestamp - a.timestamp
+    })
+    .filter((v, _i, arr) => {
+      return v.index >= arr.length - showAmount.value
+    }),
+)
+
+async function setHistoryIndex(index: number) {
+  if (!adapter.setHistoryIndex) {
+    return
+  }
+  if (index !== currentMutationIndex.value) {
+    await mutateWithLoadingState(() => adapter.setHistoryIndex!(index))
+  }
+}
+
+async function setMutationItemStatus(index: number, status: boolean) {
+  if (!adapter.setMutationItemStatus) {
+    return
+  }
+  await mutateWithLoadingState(() =>
+    adapter.setMutationItemStatus!(index, status),
+  )
+}
+</script>
--- src/runtime/components/Edit/Features/History/index.vue
+++ src/runtime/components/Edit/Features/History/index.vue
@@ -12,84 +12,7 @@
     icon="history"
     weight="-100"
   >
-    <div class="bk bk-history bk-control">
-      <ul v-if="mapped.length">
-        <li
-          v-for="item in mapped"
-          :key="item.index"
-          :class="{
-            'bk-is-not-active': item.index > currentMutationIndex,
-            'bk-is-active': item.index === currentMutationIndex,
-            'bk-is-disabled': !item.enabled,
-            'bk-is-applied': item.index < currentMutationIndex && item.enabled,
-          }"
-        >
-          <button
-            :disabled="!canEdit"
-            class="bk-history-item-button"
-            @click="setHistoryIndex(item.index)"
-          >
-            <div>
-              <div>
-                <strong>{{ item.mutation.plugin?.label }}</strong>
-              </div>
-              <RelativeTime
-                v-if="item.timestamp"
-                v-slot="{ formatted }"
-                :timestamp="item.timestamp"
-              >
-                <em>{{ formatted }}</em>
-              </RelativeTime>
-            </div>
-          </button>
-          <div v-if="canSetStatus" class="bk-history-item-actions">
-            <button
-              @click.prevent="setMutationItemStatus(item.index, !item.enabled)"
-            >
-              <Icon name="close" />
-            </button>
-          </div>
-        </li>
-        <li v-if="totalMutations > showAmount" class="bk-history-load-more">
-          <button class="bk-history-item-button" @click="showAmount += 100">
-            <strong
-              >{{
-                $t('historyShowMore', 'Show @count more').replace(
-                  '@count',
-                  Math.min(totalMutations - showAmount, 100).toString(),
-                )
-              }}
-            </strong>
-          </button>
-        </li>
-        <li
-          class="bk-is-last"
-          :class="[
-            currentMutationIndex === -1 ? 'bk-is-active' : 'bk-is-applied',
-            { 'bk-has-shadow': !scrolledToEnd },
-          ]"
-        >
-          <button class="bk-history-item-button" @click="setHistoryIndex(-1)">
-            <div>
-              <strong>{{
-                $t('historyCurrentRevision', 'Current revision')
-              }}</strong>
-            </div>
-            <!-- @TODO: Pass in the timestamp of the entity's latest revision. -->
-            <!-- <RelativeTime -->
-            <!--   v-if="item.timestamp" -->
-            <!--   :timestamp="item.timestamp" -->
-            <!--   v-slot="{ formatted }" -->
-            <!-- > -->
-            <!--   <div>{{ formatted }}</div> -->
-            <!-- </RelativeTime> -->
-          </button>
-        </li>
-      </ul>
-      <div v-else class="bk-history-empty-message">
-        {{ $t('historyEmpty', 'There are now changes yet.') }}
-      </div>
-    </div>
+    <HistoryList :scrolled-to-end="scrolledToEnd" />
   </PluginSidebar>
 
   <PluginToolbarButton
@@ -120,7 +43,6 @@
 
 <script lang="ts" setup>
 import {
-  ref,
   computed,
   useBlokkli,
   watch,
@@ -129,8 +51,7 @@ import {
   onBeforeUnmount,
 } from '#imports'
 import { PluginSidebar, PluginToolbarButton } from '#blokkli/plugins'
-import { RelativeTime, Icon } from '#blokkli/components'
-import type { MutationItem } from '#blokkli/types'
+import HistoryList from './List/index.vue'
 
 const { adapter, settings } = defineBlokkliFeature({
   id: 'history',
@@ -152,71 +73,23 @@ const { adapter, settings } = defineBlokkliFeature({
 
 const { state, $t, ui } = useBlokkli()
 
-const canSetStatus = !!adapter.setMutationItemStatus
+const { mutations, currentMutationIndex, mutateWithLoadingState } = state
 
-const { mutations, currentMutationIndex, canEdit, mutateWithLoadingState } =
-  state
-
-const showAmount = ref(50)
 const useMouseForHistory = computed(() => settings.value.useMouseButtons)
 const canUndo = computed(() => currentMutationIndex.value >= 0)
 const canRedo = computed(
   () => currentMutationIndex.value < mutations.value.length - 1,
 )
-const totalMutations = computed(() => mutations.value.length)
 
-watch(totalMutations, (newTotal, previousTotal) => {
-  if (newTotal !== previousTotal) {
-    showAmount.value = 50
-  }
-})
-
-type HistoryItem = {
-  index: number
-  mutation: MutationItem
-  timestamp: number
-  enabled: boolean
-}
-
-const mapped = computed<HistoryItem[]>(() =>
-  mutations.value
-    .map((mutation, index) => {
-      return {
-        index,
-        mutation,
-        timestamp: mutation.timestamp ? Number.parseInt(mutation.timestamp) : 0,
-        enabled: mutation.enabled !== false,
-      }
-    })
-    .sort((a, b) => {
-      return b.timestamp - a.timestamp
-    })
-    .filter((v, _i, arr) => {
-      return v.index >= arr.length - showAmount.value
-    }),
-)
-
-async function setHistoryIndex(index: number) {
-  if (index !== currentMutationIndex.value) {
-    await mutateWithLoadingState(() => adapter.setHistoryIndex(index))
-  }
-}
-
-const undo = () =>
+function undo() {
   mutateWithLoadingState(() =>
     adapter.setHistoryIndex(currentMutationIndex.value - 1),
   )
-const redo = () =>
+}
+
+function redo() {
   mutateWithLoadingState(() =>
     adapter.setHistoryIndex(currentMutationIndex.value + 1),
-  )
-
-async function setMutationItemStatus(index: number, status: boolean) {
-  if (!adapter.setMutationItemStatus) {
-    return
-  }
-  await mutateWithLoadingState(() =>
-    adapter.setMutationItemStatus!(index, status),
   )
 }
 
