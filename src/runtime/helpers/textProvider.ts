a6a0f72d176d220ea9fb2c8bba1fc2433c0171d1
Jan Hug <me@dulnan.net>
2025-10-04 10:55:47 +0200

    chore: add missing translations

--- src/runtime/helpers/textProvider.ts
+++ src/runtime/helpers/textProvider.ts
@@ -7,20 +7,36 @@ export type TextProvider = (key: string, defaultValue?: string) => string
 
 type TranslationLanguage = keyof typeof translations
 
+const DEBUG_SWISS_GERMAN = false
+
 export default function (context?: ComputedRef<AdapterContext>): TextProvider {
   const language = computed<TranslationLanguage>(() => {
     if (forceDefaultLanguage) {
       return defaultLanguage as TranslationLanguage
     }
+
     if (
       context?.value.language &&
       (translations as any)[context.value.language]
     ) {
       return context.value.language as TranslationLanguage
     }
+
     return defaultLanguage as TranslationLanguage
   })
+
   const currentTranslations = computed<any>(() => {
+    const today = new Date()
+    const isAprilFirst = today.getMonth() === 3 && today.getDate() === 1
+
+    // April Fools easter egg: Use Swiss German for German speakers.
+    if (
+      (isAprilFirst && language.value === 'de') ||
+      (DEBUG_SWISS_GERMAN && import.meta.dev)
+    ) {
+      return translations.gsw_CH || translations[language.value] || {}
+    }
+
     return translations[language.value] || {}
   })
 
--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,11 +1,13 @@
 import de from './de.json'
 import fr from './fr.json'
 import it from './it.json'
+import gsw_CH from './gsw_CH.json'
 
 const translations = {
   de,
   fr,
   it,
+  gsw_CH,
 }
 
 export default translations
