3e7289d4062416bce6ad52d5ba57cb75ffd3ea03
Jan Hug <me@dulnan.net>
2025-12-26 14:46:56 +0100

    refactor: move generated CSS to editor folder

--- src/build/templates/definitions/styles.ts
+++ src/build/templates/definitions/styles.ts
@@ -2,7 +2,7 @@ import { defineFileTemplate } from '../defineTemplate'
 
 export default defineFileTemplate('styles.css', (ctx) => {
   const cssFilePath = ctx.helper.resolvers.module.resolve(
-    './runtime/css/output.css',
+    './runtime/editor/css/output.css',
   )
   return `
 @import url("${cssFilePath}");
