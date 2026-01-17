d684fd0a37cd3223655400d6f45ddf9cb76b3271
Jan Hug <me@dulnan.net>
2026-01-17 08:09:25 +0100

    fix: render fallback in Icon component

--- src/runtime/editor/components/Icon/index.vue
+++ src/runtime/editor/components/Icon/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="bk-icon" :class="'bk-icon-' + name" v-html="markup" />
+  <div
+    v-if="markup"
+    class="bk-icon"
+    :class="'bk-icon-' + name"
+    v-html="markup"
+  />
+  <div v-else class="bk-icon" :class="'bk-icon-missing'">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        d="M10,19H13V22H10V19M12,2C17.35,2.22 19.68,7.62 16.5,11.67C15.67,12.67 14.33,13.33 13.67,14.17C13,15 13,16 13,17H10C10,15.33 10,13.92 10.67,12.92C11.33,11.92 12.67,11.33 13.5,10.67C15.92,8.43 15.32,5.26 12,5A3,3 0 0,0 9,8H6A6,6 0 0,1 12,2Z"
+      />
+    </svg>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -12,10 +24,12 @@ const props = defineProps<{
 
 const app = useBlokkli(true)
 
-const markup = computed<string>(() =>
-  (app?.icons.icons.value[props.name] ?? '')
+const iconMarkup = computed<string|null>(() => app?.icons.icons.value[props.name] ?? '')
+
+const markup = computed<string|null>(() =>
+  iconMarkup.value ? (iconMarkup.value)
     .replace('width="48"', '')
-    .replace('height="48"', ''),
+    .replace('height="48"', '') : null,
 )
 </script>
 
